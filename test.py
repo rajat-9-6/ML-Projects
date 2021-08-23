0ef94b41a6a92ebecb34361e3451f6abbab1ac4a
Sagar Jain <71249634+SagarJ17@users.noreply.github.com>
2021-08-23 15:26:31 +0530

    new changes

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 import os
 print("testing")
-print("AKIAF6BAFJKR45SAWSZ2")
 import json 
 import sys
 print("AKIAF7BFFJKR45SAWSZ2")
 a = 10
 import argparser
-print("BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl")
-print("AKIAF6BAFJKR45SAWSZ2")
